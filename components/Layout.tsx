de608f8ce5431561ad5c4c72d60396b9fe8bdcaf
alejandrosb8 <alejandrosanchezb08@gmail.com>
2022-10-10 21:41:20 -0400

    feat: folder structure and Layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Head from 'next/head';
+
+type Props = {
+  children?: React.ReactNode;
+  title?: string;
+};
+
+function Layout({ children, title = 'Postdata - Ecommerce' }: Props) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
