7e736695d9a0d82b2c284b70fabe969a16cbbcf6
alejandrosb8 <alejandrosanchezb08@gmail.com>
2022-10-11 21:13:11 -0400

    feat: layout improvement

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import { AppShell } from '@mantine/core';
 
 type Props = {
   children?: React.ReactNode;
@@ -8,14 +9,14 @@ type Props = {
 
 function Layout({ children, title = 'Postdata - Ecommerce' }: Props) {
   return (
-    <div>
+    <>
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      {children}
-    </div>
+      <AppShell>{children}</AppShell>
+    </>
   );
 }
 
