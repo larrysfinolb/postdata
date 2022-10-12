54547b34f74741314754b93241571c59bd92ce79
alejandrosb8 <alejandrosanchezb08@gmail.com>
2022-10-11 20:45:00 -0400

    feat: setup Mantine UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: 'light',
+        fontFamily: 'Source Serif Pro, Work Sans',
+        colors: {
+          green: ['#006854'],
+          yellow: ['#FCB84C'],
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
 export default MyApp;
