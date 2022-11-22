e020012ba37602f56f39d25496d20bdc363d344b
Alejandro Sanchez <alejandrosanchezb08@gmail.com>
2022-11-22 01:02:57 -0400

    feat: buy book page complete

--- pages/buy/book/[bid].tsx
+++ pages/buy/book/[bid].tsx
@@ -4,9 +4,10 @@ import Layout from 'components/Layout';
 import Header from 'components/HeaderUser';
 import Heading from 'components/Heading';
 import supabase from 'utils/supabase';
-import { Text, Button, Stack } from '@mantine/core';
+import { Text, Button, Stack, Modal, Group } from '@mantine/core';
 import styled from '@emotion/styled';
 import colors from 'utils/colors';
+import Link from 'next/link';
 
 const ColoredText = styled.span`
   color: ${colors.green};
@@ -22,37 +23,116 @@ const StyledContainer = styled.div`
 
 type Props = {};
 
-const getSessionActive: any = async () => {
-  const { data, error } = await supabase.auth.getUser();
-  return data;
-};
-
 function BuyBook({}: Props) {
   const router = useRouter();
   const { bid } = router.query;
   const [book, setBook] = React.useState<any>(null);
-  const [balance, setBalance] = React.useState<any>(null);
+  const [opened, setOpened] = React.useState(false);
+  const [isLoading, setLoading] = React.useState<any>(true);
+  const [hasBought, setHasBought] = React.useState<any>(false);
+  const [user, setUser] = React.useState<any>(null);
+  const [error, setError] = React.useState<any>(null);
 
   React.useEffect(() => {
-    const getBook = async () => {
-      const session = await getSessionActive();
-      !session.user && router.push('/login');
+    const getUser = async () => {
+      setLoading(true);
+      const { data: userData, error } = await supabase.auth.getUser();
+      error && router.push('/login');
+      if (userData?.user) {
+        const { data: clients, error: errorClients } = await supabase
+          .from('clients')
+          .select('*')
+          .eq('email', userData.user.email);
 
-      const { data, error } = await supabase
+        if (clients) {
+          setUser(clients[0]);
+        }
+      }
+    };
+    const getBook = async () => {
+      const { data: bookData, error } = await supabase
         .from('books')
         .select('*')
         .eq('id', bid);
 
-      if (data) {
-        console.log(session);
-        setBook(data);
+      if (bookData) {
+        setBook(bookData[0]);
       }
     };
+    getUser();
     getBook();
   }, [bid]);
 
+  const handleBuy = async () => {
+    if (user.balance - book.price < 0) {
+      setError('Saldo insuficiente');
+    } else {
+      const { data: updateClients, error } = await supabase
+        .from('clients')
+        .update({ balance: user.balance - book.price })
+        .eq('email', user.email);
+
+      const { data: updateBooks, error: booksError } = await supabase
+        .from('books')
+        .update({ copies: book.copies - 1 })
+        .eq('id', book.id);
+
+      setError('Gracias por su compra');
+    }
+
+    setHasBought(true);
+  };
+
   return (
     <Layout Header={<Header />}>
+      {hasBought ? (
+        <Modal
+          opened={opened}
+          onClose={() => setOpened(true)}
+          centered
+          closeOnClickOutside={false}
+          closeOnEscape={false}
+          withCloseButton={false}
+          title={error}
+        >
+          <Link href="/" passHref>
+            <Button color="green" variant="outline" fullWidth component="a">
+              Ir a inicio
+            </Button>
+          </Link>
+        </Modal>
+      ) : (
+        <Modal
+          opened={opened}
+          onClose={() => setOpened(false)}
+          centered
+          closeOnClickOutside={false}
+          closeOnEscape={false}
+          withCloseButton={false}
+          title="¿Estás seguro de que quieres comprar este libro?"
+        >
+          <Stack style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+            <Button
+              color="red"
+              variant="outline"
+              fullWidth
+              component="a"
+              onClick={() => setOpened(false)}
+            >
+              No
+            </Button>
+            <Button
+              color="green"
+              variant="outline"
+              fullWidth
+              component="a"
+              onClick={handleBuy}
+            >
+              Sí
+            </Button>
+          </Stack>
+        </Modal>
+      )}
       <Heading order={1} styles={{ textAlign: 'center' }}>
         Comprar libro
       </Heading>
@@ -61,20 +141,30 @@ function BuyBook({}: Props) {
         <ColoredText>{`"${book?.title}"`}</ColoredText>
       </Text>
       <StyledContainer>
-        <Stack>
-          <Text align="center" weight="bold" color="dark" size={14}>
-            Saldo actual:{' '}
-          </Text>
-          <Text align="center" weight="bold" color="dark" size={14}>
-            Precio del libro:{' '}
-          </Text>
-          <Text align="center" weight="bold" color="dark" size={14}>
-            Saldo restante:{' '}
-          </Text>
-          <Button fullWidth style={{ backgroundColor: '#FCB84C' }}>
-            Comprar
-          </Button>
-        </Stack>
+        {book && user && (
+          <Stack>
+            <Text align="center" weight="bold" color="dark" size={14}>
+              {`Saldo actual: ${user.balance}`}
+            </Text>
+            <Text align="center" weight="bold" color="dark" size={14}>
+              {`Precio del libro: ${book.price}`}
+            </Text>
+            <Text align="center" weight="bold" color="dark" size={14}>
+              {`Saldo después de la compra: ${
+                user.balance - book.price < 0
+                  ? 'Saldo insuficiente'
+                  : user.balance - book.price
+              }`}
+            </Text>
+            <Button
+              onClick={() => setOpened(true)}
+              fullWidth
+              style={{ backgroundColor: '#FCB84C' }}
+            >
+              Comprar
+            </Button>
+          </Stack>
+        )}
       </StyledContainer>
     </Layout>
   );
